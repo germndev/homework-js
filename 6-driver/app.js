f18c54d8b6b526acf08fe46a3b449469d6dca994
German <germanpruvl@gmail.com>
2026-02-22 15:22:45 +0300

    fix synt

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -14,4 +14,4 @@ isDrunk - пьян
 */
 
 const hasLicence = true, age = 18, isDrunk = false;
-console.log(hasLicence && age >= 18 && !isDrunk ? 'может' : 'не может')
+console.log(hasLicence && age >= 18 && !isDrunk ? 'может' : 'не может');
